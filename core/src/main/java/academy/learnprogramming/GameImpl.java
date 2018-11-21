70c59c58193657ae4251c006a2731ffc0c8ac4ef
Sandro <sandro.almeida@dotbuild.com.br>
2018-11-21 14:49:52 -0800

    Add Lombok dependency and simplify same code

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -1,30 +1,33 @@
 package academy.learnprogramming;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
-    // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
-
     // == fields ==
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGenerator;
 
     private final int guessCount;
-
     private int number;
-    private int guess;
     private int smallest;
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter
+    private int guess;
 
     // == constructors ==
     @Autowired
@@ -52,39 +55,6 @@ public class GameImpl implements Game {
 
     // == public methods ==
     @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() { return guessCount; }
-
-    @Override
     public void check() {
         checkValidNumberRange();
         if(validNumberRange){
@@ -96,11 +66,6 @@ public class GameImpl implements Game {
             }
         }
         remainingGuesses--;
-    }
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
     @Override
@@ -117,5 +82,4 @@ public class GameImpl implements Game {
     private void checkValidNumberRange(){
         validNumberRange = (guess >= smallest) && (guess <= biggest);
     }
-
 }
