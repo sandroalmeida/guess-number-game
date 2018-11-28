c7a828520e92cc053d02f35bfb02a25be3238d4a
Sandro <sandro.almeida@dotbuild.com.br>
2018-11-28 14:49:54 -0800

    Add more web configurations

---
+++ web/src/main/java/info/sandroalmeida/util/ViewNames.java
@@ -0,0 +1,10 @@
+package info.sandroalmeida.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames(){}
+}
