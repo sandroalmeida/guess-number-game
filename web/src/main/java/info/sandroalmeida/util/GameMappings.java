d993960bad0ebc36f48a37839d3c6c93ecfac112
Sandro <sandro.almeida@dotbuild.com.br>
2018-11-29 12:33:53 -0800

    Implemented GameOver view and restart game functions. Implemented constants to views instead hardcode using thymeleaf preprocessing

--- web/src/main/java/info/sandroalmeida/util/GameMappings.java
+++ web/src/main/java/info/sandroalmeida/util/GameMappings.java
@@ -5,6 +5,8 @@ public final class GameMappings {
     // == constants ==
     public static final String PLAY = "play";
     public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+    public static final String RESTART = "restart";
+    public static final String HOME = "/";
 
     // == constructors ==
     private GameMappings(){}
